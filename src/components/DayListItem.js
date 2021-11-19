a05834c2f20fafb126732ed77cedd39321284dae
LloydDub <lloydrww@gmail.com>
2021-11-19 11:50:11 -0700

    day list

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,20 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
+
+  const formatSpots = function (spotsNum) {
+    if (spotsNum === 0) {
+      return "no spots remaining";
+    } else if (spotsNum === 1) {
+      return `${spotsNum} spot remaining`;
+    } else {
+      return `${spotsNum} spots remaining`;
+    }
+  };
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
